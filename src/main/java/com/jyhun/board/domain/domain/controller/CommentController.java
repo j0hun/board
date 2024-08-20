b79dab3946bafa11b86ba3e300401692fc2e2096
j0hun <dudgns3681@gmail.com>
2024-08-20 19:29:16 +0900

    FEAT: 댓글 생성 기능 구현
    - POST /api/comments 엔드포인트를 추가하여 댓글 생성 기능을 구현
    - CommentRequestDTO를 사용하여 요청 본문을 수신
    - CommentResponseDTO를 반환하여 생성된 댓글의 정보를 클라이언트에 전달
    - CommentService에 addComment(CommentRequestDTO commentRequestDTO) 메서드를 구현하여 댓글 생성

--- src/main/java/com/jyhun/board/domain/domain/controller/CommentController.java
+++ src/main/java/com/jyhun/board/domain/domain/controller/CommentController.java
@@ -1,14 +1,13 @@
 package com.jyhun.board.domain.domain.controller;
 
+import com.jyhun.board.domain.domain.dto.CommentRequestDTO;
 import com.jyhun.board.domain.domain.dto.CommentResponseDTO;
 import com.jyhun.board.domain.domain.service.CommentService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,6 +25,13 @@ public class CommentController {
         List<CommentResponseDTO> comments = commentService.findComments();
         log.info("댓글 목록 조회 성공");
         return new ResponseEntity<>(comments, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<CommentResponseDTO> postComment(@RequestBody CommentRequestDTO commentRequestDTO) {
+        log.info("댓글 생성 요청, 생성 데이터: {}", commentRequestDTO);
+        CommentResponseDTO commentResponseDTO = commentService.addComment(commentRequestDTO);
+        return new ResponseEntity<>(commentResponseDTO, HttpStatus.CREATED);
     }
 
 }
--- src/main/java/com/jyhun/board/domain/domain/service/CommentService.java
+++ src/main/java/com/jyhun/board/domain/domain/service/CommentService.java
@@ -1,5 +1,6 @@
 package com.jyhun.board.domain.domain.service;
 
+import com.jyhun.board.domain.domain.dto.CommentRequestDTO;
 import com.jyhun.board.domain.domain.dto.CommentResponseDTO;
 import com.jyhun.board.domain.domain.entity.Comment;
 import com.jyhun.board.domain.domain.repository.CommentRepository;
@@ -21,13 +22,26 @@ public class CommentService {
     @Transactional(readOnly = true)
     public List<CommentResponseDTO> findComments() {
         log.info("findComments 메서드 호출");
+
         List<Comment> commentList = commentRepository.findAll();
         List<CommentResponseDTO> commentResponseDTOList = new ArrayList<>();
         for (Comment comment : commentList) {
             CommentResponseDTO commentResponseDTO = CommentResponseDTO.toDTO(comment);
             commentResponseDTOList.add(commentResponseDTO);
         }
+
         return commentResponseDTOList;
+    }
+
+    @Transactional
+    public CommentResponseDTO addComment(CommentRequestDTO commentRequestDTO){
+        log.info("addComment 메서드 호출");
+
+        Comment comment = commentRequestDTO.toEntity();
+        Comment savedComment = commentRepository.save(comment);
+
+        log.info("댓글 추가 완료, 댓글 ID: {}", savedComment.getId());
+        return CommentResponseDTO.toDTO(savedComment);
     }
 
 }
