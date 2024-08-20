5e45584e5489ee1feb63a27211df7e7ec5f8966d
j0hun <dudgns3681@gmail.com>
2024-08-20 19:24:01 +0900

    FEAT: 댓글 목록 조회 기능 구현
    - GET /api/comments 엔드포인트를 CommentController에 추가하여 댓글 목록 조회
    - CommentService에 findComments() 메서드를 구현하여 댓글 목록 조회
    - CommentResponseDTO를 데이터 전송을 위한 객체로 사용

--- src/main/java/com/jyhun/board/domain/domain/controller/CommentController.java
+++ src/main/java/com/jyhun/board/domain/domain/controller/CommentController.java
@@ -1,9 +1,31 @@
 package com.jyhun.board.domain.domain.controller;
 
+import com.jyhun.board.domain.domain.dto.CommentResponseDTO;
+import com.jyhun.board.domain.domain.service.CommentService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/comments")
+@Slf4j
 public class CommentController {
+
+    private final CommentService commentService;
+
+    @GetMapping
+    public ResponseEntity<List<CommentResponseDTO>> getComments() {
+        log.info("댓글 목록 조회 요청");
+        List<CommentResponseDTO> comments = commentService.findComments();
+        log.info("댓글 목록 조회 성공");
+        return new ResponseEntity<>(comments, HttpStatus.OK);
+    }
+
 }
--- src/main/java/com/jyhun/board/domain/domain/service/CommentService.java
+++ src/main/java/com/jyhun/board/domain/domain/service/CommentService.java
@@ -1,10 +1,33 @@
 package com.jyhun.board.domain.domain.service;
 
+import com.jyhun.board.domain.domain.dto.CommentResponseDTO;
+import com.jyhun.board.domain.domain.entity.Comment;
+import com.jyhun.board.domain.domain.repository.CommentRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class CommentService {
+
+    private final CommentRepository commentRepository;
+
+    @Transactional(readOnly = true)
+    public List<CommentResponseDTO> findComments() {
+        log.info("findComments 메서드 호출");
+        List<Comment> commentList = commentRepository.findAll();
+        List<CommentResponseDTO> commentResponseDTOList = new ArrayList<>();
+        for (Comment comment : commentList) {
+            CommentResponseDTO commentResponseDTO = CommentResponseDTO.toDTO(comment);
+            commentResponseDTOList.add(commentResponseDTO);
+        }
+        return commentResponseDTOList;
+    }
 
 }
