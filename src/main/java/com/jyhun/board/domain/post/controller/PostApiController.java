52727edcc5e1aa208113b751689994061f6b539c
j0hun <dudgns3681@gmail.com>
2024-08-26 13:29:27 +0900

    FEAT: 게시글 목록 조회 및 상세 조회 페이지 구현
    - PostPageController 클래스 추가하여 게시글 목록과 상세 조회 데이터를 모델에 담아 타임리프 뷰로 전달
    - 타임리프를 사용하여 모델 데이터를 뷰에 렌더링하는 페이지 구현

--- src/main/java/com/jyhun/board/domain/post/controller/PostApiController.java
+++ src/main/java/com/jyhun/board/domain/post/controller/PostApiController.java
@@ -2,13 +2,9 @@ package com.jyhun.board.domain.post.controller;
 
 import com.jyhun.board.domain.post.dto.PostRequestDTO;
 import com.jyhun.board.domain.post.dto.PostResponseDTO;
-import com.jyhun.board.domain.post.dto.PostSearchDTO;
 import com.jyhun.board.domain.post.service.PostService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,30 +13,9 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api")
 @RequiredArgsConstructor
 @Slf4j
-public class PostController {
+public class PostApiController {
 
     private final PostService postService;
-
-    @GetMapping("/boards/{boardId}/posts")
-    public ResponseEntity<Page<PostResponseDTO>> getPosts(@PathVariable Long boardId,
-                                                          @RequestParam(required = false) String searchKey,
-                                                          @RequestParam(required = false) String searchValue,
-                                                          @PageableDefault Pageable pageable) {
-        PostSearchDTO postSearchDTO = new PostSearchDTO(searchKey, searchValue);
-        log.info("게시글 목록 페이징 조회, 검색 조건: {}, 페이지: {}", postSearchDTO, pageable);
-        Page<PostResponseDTO> posts = postService.findPosts(boardId,postSearchDTO, pageable);
-        return new ResponseEntity<>(posts, HttpStatus.OK);
-    }
-
-    @GetMapping("/posts/{postId}")
-    public ResponseEntity<PostResponseDTO> getPostById(@PathVariable Long postId) {
-        log.info("ID {}의 게시글 조회 요청", postId);
-        PostResponseDTO post = postService.findPostById(postId);
-        log.info("ID {}의 게시글 조회 성공", postId);
-        postService.increaseViewCount(postId);
-        log.info("ID {}의 조회수 증가 성공", postId);
-        return new ResponseEntity<>(post, HttpStatus.OK);
-    }
 
     @PostMapping("/boards/{boardId}/posts")
     public ResponseEntity<PostResponseDTO> postPost(@PathVariable Long boardId,
---
+++ src/main/java/com/jyhun/board/domain/post/controller/PostPageController.java
@@ -0,0 +1,54 @@
+package com.jyhun.board.domain.post.controller;
+
+import com.jyhun.board.domain.post.dto.PostResponseDTO;
+import com.jyhun.board.domain.post.dto.PostSearchDTO;
+import com.jyhun.board.domain.post.service.PostService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequiredArgsConstructor
+@Slf4j
+public class PostPageController {
+
+    private final PostService postService;
+
+    @RequestMapping("/boards/{boardId}")
+    public String getPosts(Model model,
+                           @PathVariable Long boardId,
+                           @RequestParam(required = false) String searchKey,
+                           @RequestParam(required = false) String searchValue,
+                           @PageableDefault(size = 10) Pageable pageable) {
+        PostSearchDTO postSearchDTO = new PostSearchDTO(searchKey, searchValue);
+        log.info("게시글 목록 페이징 조회, 검색 조건: {}, 페이지: {}", postSearchDTO, pageable);
+
+        Page<PostResponseDTO> posts = postService.findPosts(boardId, postSearchDTO, pageable);
+
+        model.addAttribute("posts", posts);
+        model.addAttribute("boardId", boardId);
+        model.addAttribute("postSearchDTO", postSearchDTO);
+        model.addAttribute("maxPage", 5);
+
+        return "posts/posts";
+    }
+
+
+    @RequestMapping("/posts/{postId}")
+    public String getPostById(Model model, @PathVariable Long postId) {
+        log.info("ID {}의 게시글 조회 요청", postId);
+        PostResponseDTO post = postService.findPostById(postId);
+        log.info("ID {}의 게시글 조회 성공", postId);
+        postService.increaseViewCount(postId);
+        log.info("ID {}의 조회수 증가 성공", postId);
+        model.addAttribute("post",post);
+        return "posts/post";
+    }
+}
