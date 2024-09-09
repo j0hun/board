ecf83880d5c87ef9372b88c6567eb3f7140649ff
j0hun <dudgns3681@gmail.com>
2024-09-09 17:03:58 +0900

    FEAT: JWT 적용을 위한 Config 및 Filter 추가
    - JwtProvider 추가하여 JWT 생성 및 검증 로직 구현
    - JwtFilter 추가하여 요청 필터링 기능 구현
    - application.yml에 JWT secret 키 설정 추가

---
+++ src/main/java/com/jyhun/board/global/config/JwtProvider.java
@@ -0,0 +1,69 @@
+package com.jyhun.board.global.config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtProvider {
+
+    private static final long TOKEN_EXPIRATION = 3600000L; // 1시간
+    private static final String CLAIM_USERNAME = "username";
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    private Key key;
+
+    @PostConstruct
+    public void init() {
+        key = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public String createToken(Authentication authentication) {
+        Date now = new Date();
+        return Jwts.builder()
+                .setIssuer(authentication.getName())
+                .setSubject("JWT TOKEN")
+                .claim(CLAIM_USERNAME, authentication.getName())
+                .setIssuedAt(now)
+                .setExpiration(new Date(now.getTime() + TOKEN_EXPIRATION))
+                .signWith(key, SignatureAlgorithm.HS512)
+                .compact();
+    }
+
+    public Authentication getAuthentication(String token) {
+        Claims claims = parseClaims(token);
+        String username = String.valueOf(claims.get(CLAIM_USERNAME));
+        return new UsernamePasswordAuthenticationToken(username,token);
+    }
+
+    public boolean validateToken(String token) {
+        try{
+            parseClaims(token);
+            return true;
+        }catch (Exception e){
+            return false;
+        }
+    }
+
+    private Claims parseClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+}
---
+++ src/main/java/com/jyhun/board/global/filter/JwtFilter.java
@@ -0,0 +1,47 @@
+package com.jyhun.board.global.filter;
+
+import com.jyhun.board.global.config.JwtProvider;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.GenericFilterBean;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+@Slf4j
+public class JwtFilter extends GenericFilterBean {
+
+    private static final String BEARER_PREFIX = "Bearer ";
+    private static final String header = "Authorization";
+
+    private final JwtProvider jwtProvider;
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+        String token = resolveToken(httpServletRequest);
+        if(StringUtils.hasText(token) && jwtProvider.validateToken(token)) {
+            Authentication authentication = jwtProvider.getAuthentication(token);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            log.debug("Security Context에 {} 인증 정보를 저장했습니다.", authentication.getName());
+        }
+        filterChain.doFilter(servletRequest,servletResponse);
+    }
+
+    private String resolveToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader(header);
+
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith(BEARER_PREFIX)) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+}
